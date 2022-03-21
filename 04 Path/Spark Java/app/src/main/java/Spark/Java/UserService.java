3a8cae2677153d40ed726b37d0204db2906e86a9
Simon Schwab <sim.schwab@me.com>
2022-03-21 14:52:46 +0100

    added docker exapmle from lesson 8

--- 04 Path/Spark Java/app/src/main/java/Spark/Java/UserService.java
+++ 04 Path/Spark Java/app/src/main/java/Spark/Java/UserService.java
@@ -7,33 +7,33 @@ import javax.servlet.UnavailableException;
 public interface UserService {
   
     public static void addUser (User user) {
-        // TODO Auto-generated method stub
+        // Auto-generated method stub
         
     }
     
     public static Collection<User> getUsers () {
-        // TODO Auto-generated method stub
+        // Auto-generated method stub
         return null;
     }
     
     public static User getUser (String id) {
-        // TODO Auto-generated method stub
+        // Auto-generated method stub
         return null;
     }
     
     public static User editUser (User user) 
      throws UnavailableException {
-        // TODO Auto-generated method stub
+        // Auto-generated method stub
         return null;
     }
     
     public static void deleteUser (String id) {
-        // TODO Auto-generated method stub
+        // Auto-generated method stub
         
     }
     
     public static boolean userExist (String id) {
-        // TODO Auto-generated method stub
+        // Auto-generated method stub
         return false;
     }
 }
--- 08 Container/README.md
+++ 08 Container/README.md
@@ -1,7 +1,22 @@
-# 08
+# 08 Docker
 
-## 
+## example from lesson
 
+2 Containers (Wordpress & MySQL)
+navigate to /example tutorial
 
-## 
+```
+$ docker-compose up
+```
 
+frontend wordpress: http://localhost:8080
+
+&nbsp;
+
+## own example
+
+navigate to /own example
+
+```
+$ docker-compose up
+```
