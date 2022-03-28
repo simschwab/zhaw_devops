c17b78a996ba908a8d38d56dfe7ef9527635825b
Mario Locher <lochemar@students.zhaw.ch>
2022-03-28 10:35:51 +0200

    Convert Test 1 (new)

--- 05 Unit Tests/junit5/src/main/java/NumberConverter.java
+++
@@ -1,6 +0,0 @@
-public class NumberConverter {
-
-    public String callForAction() {
-        return "Dog";
-    }
-}
--- 05 Unit Tests/junit5/src/test/java/ConvertTest1.java
+++ 05 Unit Tests/junit5/src/test/java/ConvertTest1.java
@@ -2,7 +2,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.assertj.core.api.Assert;
+
+//import org.assertj.core.api.Assert.*;
+
+
+
+
 
 
 class ConvertTest1 {
@@ -10,7 +15,8 @@ class ConvertTest1 {
     @Test
     public void numberConverter1() {
         NumberConverter numberConverter = new NumberConverter();
-        Assert.assertEquals("X", numberConverter.convert(10));
+        
+        assertThat("X").isEqualTo(numberConverter.convert(10));
                 
     }
 
