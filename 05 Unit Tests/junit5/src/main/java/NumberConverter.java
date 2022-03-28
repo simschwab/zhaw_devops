e26f657e4ddbe1605b60ccf32578fd7b40568e2b
Mario Locher <lochemar@students.zhaw.ch>
2022-03-28 11:11:40 +0200

    Number Converter 2

--- 05 Unit Tests/junit5/src/main/java/NumberConverter.java
+++ 05 Unit Tests/junit5/src/main/java/NumberConverter.java
@@ -1,6 +1,28 @@
+import java.util.HashMap;
+
+
+
 public class NumberConverter {
     
+    String character = "X";
+
+    public String getCharacter() {
+        return character;
+    }
+
+   
+
     public String convert(int i) {
+        
+        //HashMap<String, Integer> character = new HashMap<>();
+        
+        //character.put("M", 1000);
+        
+
+
+        
+        
+        
         return "X";
     }
 }
--- 05 Unit Tests/junit5/src/test/java/ConvertTest2.java
+++ 05 Unit Tests/junit5/src/test/java/ConvertTest2.java
@@ -5,11 +5,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ConvertTest2 {
 
+    public String roman;
+
     @Test
     public void numberConverter2() {
         NumberConverter numberConverter = new NumberConverter();
         
-        String roman = NumberConverter.getCharacter(10);
+        roman = numberConverter.getCharacter();
 
         assertThat(roman).isEqualTo(numberConverter.convert(10));
                 
