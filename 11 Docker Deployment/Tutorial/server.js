ac474b05bf41dc11dd1f78cc6fb966322047d0d5
Simon Schwab <sim.schwab@me.com>
2022-05-22 09:34:33 +0200

    testing jenkins build

--- 11 Docker Deployment/Tutorial/server.js
+++ 11 Docker Deployment/Tutorial/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express(); 
 app.get('/', (req, res) => {
-    res.send('DevOps Course FS 2022, created by Simon :)'); 
+    res.send('DevOps Course FS 2022, created by Simon :) and this is a change to test automated jenkins build'); 
 });
 
 app.listen(PORT, HOST); 
